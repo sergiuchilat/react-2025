16daea31d739cdaa3c799912cafddc1e86dce0d4
sergiuchilat <chilatsergiu@gmail.com>
2025-02-21 14:21:55 +0200

    Split application in components

---
+++ src/app/components/TodoAdd.jsx
@@ -0,0 +1,8 @@
+export default function TodoAdd(){
+    return (
+        <>
+          <input placeholder="Enter your todo" />
+          <button>Add</button>
+        </>
+    );
+}
---
+++ src/app/components/TodoList.jsx
@@ -0,0 +1,16 @@
+export default function TodoList() {
+    return (
+        <ul className="todo-list">
+            <li>
+            <span className='checkbox'>v</span>
+            Sa nu uit sa maninc
+            </li>
+            <li>
+            Sa nu fac temele de acasa la web I
+            </li>
+            <li>
+            Sa nu uit sa ma duc la sala
+            </li>
+        </ul>
+    );
+}
