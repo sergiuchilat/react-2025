21245e242141db16cacb14cd3cb1f7075792e71d
sergiuchilat <chilatsergiu@gmail.com>
2025-02-21 15:56:10 +0200

    Fix the issue with @mdi/js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 //import styles from "./page.module.css";
 import React from 'react';
 import Icon from '@mdi/react';
-//import { mdiAccount, mdiBlockHelper } from '@mdi/js';
+import { mdiAccount, mdiBlockHelper } from '@mdi/js';
 import TodoAdd from '../components/todo/TodoAdd';
 import TodoList from '../components/todo/TodoList';
 import NavBar from '../components/layout/NavBar';
